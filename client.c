4cd6bf279584ab183beb8e2821913af971487482
Ethan L. Gearey <f004rwq@plank.thayer.dartmouth.edu>
2022-03-01 16:00:42 -0500

    working on parseArgs

--- client.c
+++ client.c
@@ -8,6 +8,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include "file.h"
+#include "message.h"
 // #include "player.h"
 
 // functions
@@ -24,23 +25,44 @@ static bool handleInput(void* arg);
 static bool checkInput(void* arg);
 
 // variables
-static bool spectator; // i guess this also doesn't have to be global. But if it is global and static vs just declared static in the body, is that not the same thing? TODO
+// static bool spectator; // i guess this also doesn't have to be global. But if it is global and static vs just declared static in the body, is that not the same thing? TODO
+    // i guess i can just do it without static... 
+
 static player_t* player; // can also just initialize this in join game (when you call player_new) or with handleMessage
-
-
 
 /********************* main ********************/
 int
 main(const int argc, char* argv[])
 {
+  parseArgs(
 
 
 }
 
 /******************* parseArgs *****************/
+/* Parses args for use in handleMessage and handleInput.
+ * Decides whether or not it is spectator. 
+ * Initializes message module. 
+ *
+ */
 static int 
 parseArgs(const int argc, char* argv[])
 {
+  // if message module cannot be initialized
+  if (message_init(NULL) == 0) {
+    fprintf(stderr, "could not initialize message module");
+    exit(2); 
+  }
+
+  // check arg count
+  if (argc != 3 || argc != 4) {
+    fprintf(stderr, "usage: Client can take either 2 or 3 args");
+    exit(3);
+  }
+
+
+
+
 
 }
 
