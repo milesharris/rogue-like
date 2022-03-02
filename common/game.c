0637b8b2c33603bde88e6a508d6ac91de0c26b07
Miles Harris <f004357@plank.thayer.dartmouth.edu>
2022-03-02 09:59:01 -0500

    game branch initial

---
+++ common/game.c
@@ -0,0 +1,7 @@
+/*
+ * game module implementation for CS50 nuggets game
+ * stores all information about the current state of a game
+ * used as a global variable in both server and client
+ */
+
+#include "game.h"
---
+++ common/game.h
@@ -0,0 +1,13 @@
+/*
+ * game module definition for CS50 nuggets game
+ * stores all information about the current state of a game
+ * used as a global variable in both server and client
+ */
+
+#ifndef __GAME_H
+#define __GAME_H
+
+//TODO: everything
+
+
+#endif
