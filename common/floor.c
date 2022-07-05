c5b8c3c9c5365037abb838443bfa29ac76db3f34
Miles Harris <f004357@plank.thayer.dartmouth.edu>
2022-07-04 21:50:47 -0400

    fleshed out floor some more

--- common/floor.c
+++ common/floor.c
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
-#include "game.h"
+#include "floor.h"
 #include "mem.h"
 #include "grid.h"
 #include "hashtable.h"
@@ -22,7 +22,8 @@ typedef struct floor
   int *piles;           // array of gold piles
   int remainingGold;    // gold left in the game
   int numPiles;         // number of gold piles in game
-  hashtable_t *players; // hashtable of player IDs
+  hashtable_t *players; // hashtable of player IDs on the floor
+  int numPlayers;       // number of players on the floor
   grid_t *grid;         // current floor grid
   char *mapfile;        // filepath of the in-game map
 } floor_t;
@@ -43,14 +44,14 @@ char *floor_getMapfile(floor_t *floor)
   return floor ? floor->mapfile : NULL;
 }
 
-int floor_getNumPiles(game_t *floor)
+int floor_getNumPiles(floor_t *floor)
 {
   return floor ? floor->numPiles : -1;
 }
 
 hashtable_t *floor_getPlayers(floor_t *floor)
 {
-  return game ? floor->players : NULL;
+  return floor ? floor->players : NULL;
 }
 
 int floor_getNumPlayers(floor_t *floor)
@@ -61,4 +62,67 @@ int floor_getNumPlayers(floor_t *floor)
 int floor_getRemainingGold(floor_t *floor)
 {
   return floor ? floor->remainingGold : -1;
+}
+
+/**************** setters ****************/
+
+/************** floor_setRemainingGold **************/
+/* see floor.h for details */
+bool floor_setRemainingGold(floor_t *floor, int gold)
+{
+  if (floor == NULL)
+  {
+    return false;
+  }
+
+  floor->remainingGold = gold;
+  return true;
+}
+
+/************** floor_setGrid **************/
+/* see floor.h for details */
+bool floor_setGrid(floor_t *floor, grid_t *grid)
+{
+  if (floor == NULL || grid == NULL)
+  {
+    return false;
+  }
+  else
+  {
+    grid_delete(floor->grid);
+    floor->grid = grid;
+    return true;
+  }
+}
+
+/************** floor_setNumPlayers **************/
+/* see floor.h for details */
+int floor_setNumPlayers(floor_t *floor, int numPlayers)
+{
+  if (floor == NULL)
+  {
+    return -1;
+  }
+  floor->numPlayers = numPlayers;
+  return floor->numPlayers;
+}
+
+/************** floor_setNumPiles **************/
+/* see floor.h for details */
+int floor_setNumPiles(floor_t *floor, int numPiles)
+{
+  if (floor == NULL)
+  {
+    return -1;
+  }
+  floor->numPiles = numPiles;
+  return floor->numPiles;
+}
+
+/*************** functions *****************/
+floor_t *floor_new(grid_t *grid, int *piles)
+{
+  hashtable_t *players; // tracks players on the floor
+
+  // allocate space for floor and check
 }
--- common/floor.h
+++ common/floor.h
@@ -25,4 +25,36 @@ int floor_getRemainingGold(floor_t *floor);
 char *floor_getMapfile(floor_t *floor);
 int floor_getNumPiles(floor_t *floor);
 
+/**************** setters ***************/
+/* return false on failure, true on success */
+bool floor_setRemainingGold(floor_t *floor, int gold);
+
+/* Note: the setGrid function calls grid_delete on the previous floor->grid
+ * to avoid memory leaks
+ */
+bool floor_setGrid(floor_t *floor, grid_t *grid);
+
+/* sets the number of players to the given value
+ * returns the new number on success
+ * returns -1 when game NULL or numPlayers exceeds MAXPLAYERS
+ * */
+int floor_setNumPlayers(floor_t *floor, int numPlayers);
+
+/* sets the number of gold piles in a game
+ * returns the new number
+ */
+int floor_setNumPiles(floor_t *floor, int numPiles);
+
+/*************** functions *****************/
+
+/**************** floor_new *****************/
+/* The game_new function allocates space for a new 'struct floor'
+ * it only malloc's space for itself. All other memory must be allocated before
+ * for example, a `floor` takes a non-null `grid` as a parameter
+ * so grid_new must be called on a grid before passing it to `floor`
+ * All memory allocated by the floor, its grid, and its int array
+ * are freed in floor_delete
+ */
+floor_t *floor_new(grid_t *grid, int *piles);
+
 #endif
